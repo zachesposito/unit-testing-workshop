3ae7b21f436e6b550fcbbb53c197e66c61234249
Zach Esposito <zesposito@thermatru.com>
2018-09-11 02:42:24 -0400

    Add test for top popular

--- source/UnitTestingWorkshop.Demo.UI/UnitTestingWorkshop.Demo.Tests/MovieServiceTests.cs
+++ source/UnitTestingWorkshop.Demo.UI/UnitTestingWorkshop.Demo.Tests/MovieServiceTests.cs
@@ -21,16 +21,33 @@ namespace UnitTestingWorkshop.Demo.Tests
 
             var movies = movieService.GetMovies();
 
+            Assert.Equal(5, movies.Count());
+        }
+
+        [Fact]
+        public void ShouldGetTopPopularMovies()
+        {
+            var mockMovieDataService = new Mock<IMovieDataService>();
+            mockMovieDataService.Setup(s => s.LoadMovies())
+                .Returns(CreateTestMovieCollection());
+
+            var movieService = new MovieService(mockMovieDataService.Object);
+
+            var movies = movieService.GetTopPopularMovies(3);
+
             Assert.Equal(3, movies.Count());
+            Assert.True(movies.First().Popularity == movies.Max(m => m.Popularity)); //The movie sorted first should have the highest popularity 
         }
 
         private IEnumerable<Movie> CreateTestMovieCollection()
         {
             return new Movie[]
             {
-                new Movie { Title = "Test Movie 1" },
-                new Movie { Title = "Test Movie 2" },
-                new Movie { Title = "Test Movie 3" }
+                new Movie { Title = "Test Movie 1", Popularity = 1.0f },
+                new Movie { Title = "Test Movie 2", Popularity = 2.0f },
+                new Movie { Title = "Test Movie 3", Popularity = 3.0f },
+                new Movie { Title = "Test Movie 4", Popularity = 4.0f },
+                new Movie { Title = "Test Movie 5", Popularity = 5.0f }
             };
         }
     }
